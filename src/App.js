da6ddee0b908114bb040b08494007297d87a1612
NoobSaiyan <ms19961998@gmail.com>
2020-08-27 12:47:25 +0530

    added sign in page with style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
-import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 
 function App() {
   return (
     <div>
       <Header />
-      <switch>
+      <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-      </switch>
+        <Route path="/signin" component={SignInAndSignUp} />
+      </Switch>
     </div>
   );
 }
