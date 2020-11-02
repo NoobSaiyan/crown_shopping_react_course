21513998968e9fc784d8e8c8ce175f408f8a8424
NoobSaiyan <ms19961998@gmail.com>
2020-11-02 20:26:46 +0530

    store authenticated user to our cloud firestore

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
   constructor(props) {
@@ -17,15 +17,28 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth)
 
-      console.log(user);
-    });
+        userRef.onSnapshot(snapShot =>{
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          },() => {
+            console.log(this.state)
+          })
+        })
+      }else{
+        this.setState({currentUser:userAuth})
+      }
+    })
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth();
+    this.unsubscribeFromAuth()
   }
   render() {
     return (
