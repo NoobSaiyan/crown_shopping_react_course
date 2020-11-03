194a3504555407fe82ac597d07ae9c0bd0ca75d8
NoobSaiyan <ms19961998@gmail.com>
2020-11-03 14:32:48 +0530

    added redux to cart dropdown

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux'
 
 import userReducer from './user-reducer/user.reducer'
+import cartReducer from './cart-reducer/cart.reducer'
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
