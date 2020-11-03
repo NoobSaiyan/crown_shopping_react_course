9dc2364cf34538c9d09f22e914a7cfddbf5d9f2d
NoobSaiyan <ms19961998@gmail.com>
2020-11-03 20:35:10 +0530

    adding cart styling

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -4,7 +4,7 @@ import "./preview-collection.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
-    <h1 className="title">{title}</h1>
+    <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
