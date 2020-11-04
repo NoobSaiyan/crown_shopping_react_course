7b6c5f02884ebdf5f8b5bdbddba49abcc9b60361
NoobSaiyan <ms19961998@gmail.com>
2020-11-04 17:16:22 +0530

    memoising using selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
 import { toggleCartHidden } from '../../redux/cart-reducer/cart.actions'
+import { selectCartItemsCount } from '../../redux/cart-reducer/cart.selectors'
+
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
-export default connect(null, mapDispatchToProps)(CartIcon)
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
+  });
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
